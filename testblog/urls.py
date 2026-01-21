d927508f48959cf89b07162108828757cf2251ac
worksasharak-ux <work.sasha.rak@gmail.com>
2026-01-21 21:42:54 +0300

    Дополнено создание постов

--- testblog/urls.py
+++ testblog/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', main),
-    path('user/<int:userid>', user_posts)
+    path('user/<int:userid>', user_posts),
+    path('user/create-post', create_posts),
 ]
--- testblog/views.py
+++ testblog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from .models import  Post
 
 # Create your views here.
@@ -13,3 +14,14 @@ def user_posts(request,userid):
         "posts" : Post.objects.filter(author_id=userid),
     }
     return render(request, 'main.html', context)
+
+@login_required # декоратор проверки авторизирован ли пользователь
+def create_posts(request): # метод создания поста
+    if request.method == 'POST':
+        text = request.POST.get("post_text")
+        if text:
+            Post.objects.create(post_text=text, author=request.user) # добавление поста в базу(модели)
+            return redirect("/posts/")
+    if request.method == 'GET':
+        pass
+    return render(request, 'create_post.html', )
